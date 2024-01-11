5ca01f72ecaba18a0275b848b0926782dee2b8c0
nthadrien <91905798+nthadrien@users.noreply.github.com>
2024-01-11 14:53:58 -0800

    everything

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://nticonsulti.github.io',
+    // base: '/my-repo',
+});
